c81c494e9be74eb9edbe5888046a775ad5daca8b
Dennis Angemi <dennisangemi@gmail.com>
2023-07-14 14:20:42 +0200

    added href to footer button, updated footer #46

--- assets/js/fetcher.js
+++ assets/js/fetcher.js
@@ -6,10 +6,32 @@ function replaceInnerHTMLByClass(className, innerHTML) {
    }
  }
 
+// Funzione che aggiunge un href a tutti gli elementi con una determinata classe
+function addHrefByClass(className, href)
+  {
+    var elements = document.getElementsByClassName(className);
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].href = href;
+    }
+  }
+
+// Funzione specifica per i numeri di telefono
+function populatePhoneNumberByClass(className, number_with_space)
+  {
+    var elements = document.getElementsByClassName(className);
+    var number_without_space = number_with_space.replace(/\s/g, '');
+    for (var i = 0; i < elements.length; i++) 
+      {
+        elements[i].innerHTML = `<a href="tel:+39${number_without_space}">${number_with_space}</a>`;
+      }
+  }
+
 // Input
 const jsonFile = '../../data/data.json';
 const ansaElementNumber = 0;
 const infrastruttureElementNumber = 1;
+const linkedinElementNumber = 0;
+const mapsElementNumber = 1;
  
 // Esegui una richiesta HTTP per ottenere il file JSON
 fetch(jsonFile)
@@ -17,18 +39,16 @@ fetch(jsonFile)
   .then(data =>
       {
          // Utilizza la variabile 'data' come desideri
-         console.log(data);
+         // console.log(data);
 
          // Popolo email gruppo come link
          replaceInnerHTMLByClass("group-email",`<a href="mailto:${data.email}">${data.email}</a>`);
+          
+         // Usa la funzione pupolatePhoneNumberByClass per popolare i numeri di telefono
+         populatePhoneNumberByClass("group-phone",data.phone);
+         populatePhoneNumberByClass("mobile-infrastrutture", data.company[infrastruttureElementNumber]['mobile-phone']);
+         populatePhoneNumberByClass("mobile-ansa",data.company[ansaElementNumber]['mobile-phone']);
 
-         // Popolo il numero di telefono gruppo come link
-         var elements = document.getElementsByClassName("group-phone");
-         const phone_number_without_space = data.phone.replace(/\s/g, '');
-         for (var i = 0; i < elements.length; i++) {
-            elements[i].innerHTML = `<a href="tel:+39${phone_number_without_space}">${data.phone}</a>`;
-            }
-         
          // Popola PEC ANSA con 
          replaceInnerHTMLByClass("pec-ansa",data.company[ansaElementNumber].pec);
 
@@ -40,6 +60,13 @@ fetch(jsonFile)
 
          // Popola PIVA Infrastrutture
          replaceInnerHTMLByClass("piva-infrastrutture",data.company[infrastruttureElementNumber].piva);
+
+         // Social: aggiungo href a tutti gli elementi con classe "group-github"
+         addHrefByClass("group-github",`https://github.com/${data.github_organization}`);
+         addHrefByClass("li-ansa",data.company[ansaElementNumber].social[linkedinElementNumber].url);
+         addHrefByClass("li-infrastrutture",data.company[infrastruttureElementNumber].social[linkedinElementNumber].url);
+         addHrefByClass("maps-ansa",data.company[ansaElementNumber].social[mapsElementNumber].url);
+         addHrefByClass("maps-infrastrutture",data.company[infrastruttureElementNumber].social[mapsElementNumber].url);
       })
   .catch(error => {
     console.error('Errore durante il recupero del file JSON:', error);
